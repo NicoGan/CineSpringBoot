a9707b478bea7d6f48572412c945d31d5ea8d6be
NicoGan <nicogan1707@yahoo.com.ar>
2025-06-13 11:30:05 -0300

    configuracion BaseService y BaseServiceImpl

--- src/main/java/com/example/CineSpringBoot/services/BaseService.java
+++ src/main/java/com/example/CineSpringBoot/services/BaseService.java
@@ -1,4 +1,14 @@
 package com.example.CineSpringBoot.services;
 
-public interface BaseService {
+import com.example.CineSpringBoot.entities.Base;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<E extends Base, ID extends Serializable> {
+    public List<E> findAll() throws Exception;
+    public E findById(ID id) throws Exception;
+    public E save(E entity) throws Exception;
+    public E update(ID id,E entity) throws Exception;
+    public boolean delete(ID id) throws Exception;
 }
--- src/main/java/com/example/CineSpringBoot/services/BaseServiceImpl.java
+++ src/main/java/com/example/CineSpringBoot/services/BaseServiceImpl.java
@@ -1,4 +1,14 @@
 package com.example.CineSpringBoot.services;
 
-public class BaseServiceImpl {
+import com.example.CineSpringBoot.entities.Base;
+import com.example.CineSpringBoot.repositories.BaseRepository;
+
+import java.io.Serializable;
+
+public abstract class BaseServiceImpl <E extends Base, ID extends Serializable> implements BaseService<E,ID> {
+    protected BaseRepository<E, ID> baseRepository;
+
+    public BaseServiceImpl(BaseRepository<E, ID> baseRepository) {
+        this.baseRepository = baseRepository;
+    }
 }
