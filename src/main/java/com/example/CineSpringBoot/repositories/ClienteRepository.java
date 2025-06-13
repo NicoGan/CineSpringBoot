4a28b3aa6854576443da14a2e3d389323c466eb7
NicoGan <nicogan1707@yahoo.com.ar>
2025-06-12 23:22:26 -0300

    configuracion EmpleadoRepository y ClienteRepository

--- src/main/java/com/example/CineSpringBoot/repositories/ClienteRepository.java
+++ src/main/java/com/example/CineSpringBoot/repositories/ClienteRepository.java
@@ -1,4 +1,16 @@
 package com.example.CineSpringBoot.repositories;
 
-public interface ClienteRepository {
+import com.example.CineSpringBoot.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ClienteRepository extends BaseRepository<Cliente, Long>{
+
+    List<Cliente> findByNombreContaining(String nombre);
+    Page<Cliente> findByNombreContaining(String nombre, Pageable pageable);
+    boolean existsByNombre(String nombre);
+
+
 }
--- src/main/java/com/example/CineSpringBoot/repositories/EmpleadoRepository.java
+++ src/main/java/com/example/CineSpringBoot/repositories/EmpleadoRepository.java
@@ -1,4 +1,16 @@
 package com.example.CineSpringBoot.repositories;
 
-public interface EmpleadoRepository {
+import com.example.CineSpringBoot.entities.Empleado;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmpleadoRepository extends BaseRepository<Empleado, Long> {
+    List<Empleado> findByNombre(String nombre);
+    Page<Empleado> findByNombreContaining(String nombre, Pageable pageable);
+
+    boolean existsByDni(String dni);
 }
