832edd04afc46373acce4099c375c3a06891c7ee
NicoGan <nicogan1707@yahoo.com.ar>
2025-06-12 22:59:08 -0300

    configuracion BaseRepository

--- src/main/java/com/example/CineSpringBoot/repositories/BaseRepository.java
+++ src/main/java/com/example/CineSpringBoot/repositories/BaseRepository.java
@@ -1,4 +1,11 @@
 package com.example.CineSpringBoot.repositories;
 
-public interface BaseRepository {
+import com.example.CineSpringBoot.entities.Base;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+@NoRepositoryBean
+public interface BaseRepository <E extends Base, ID extends Serializable> extends JpaRepository<E,ID> {
 }
